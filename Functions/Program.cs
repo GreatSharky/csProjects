14b554eddd2ddd08ce4467aebee452d24e07d63e
Aaro Haikonen <aaro.haikonen99@gmail.com>
2020-03-19 23:08:28 +0200

    Just some random testing

---
+++ Functions/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string iput = "";
+            Console.WriteLine("Im testing functions");
+            iput = Console.ReadLine();
+            if(iput.Length == 0){
+               noInput(); 
+            }
+            else if(iput.Length < 3){
+                shortInput();
+            }
+            else{
+                Console.WriteLine(longInput());
+            }
+        }
+
+        static void shortInput(){
+            Console.WriteLine("Your word is short");
+        }
+
+        static void noInput(){
+            Console.WriteLine("You gave no input");
+        }
+
+        static string longInput(){
+            return "Your input is long";
+        }
+    }
+}
