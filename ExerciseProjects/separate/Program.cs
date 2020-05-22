cf42154aba806fb96176407e09670babf0c4df27
Aaro Haikonen <aaro.haikonen99@gmail.com>
2020-05-22 19:39:25 +0300

    Needs the separating function. Takes inputs

---
+++ ExerciseProjects/separate/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace separate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            string sep;
+            Console.writeLine("Give a string to separate:");
+            input = Console.ReadLine();
+            Console.writeLine("Give separator. If empty, space is used:");
+            sep = Console.ReadLine();
+        }
+
+        static List<string> splitting(string input, string sep)
+        {
+            if(!input.contains(sep)){
+                Console.writeLine("There")
+            }
+        }
+    }
+}
