b59d4614f4a4a99aca3b8322bc9bac11c6ba5ce8
Aaro Haikonen <aaro.haikonen99@gmail.com>
2020-09-04 18:54:12 +0300

    Vechile parent object

--- ExerciseProjects/car2/Program.cs
+++ ExerciseProjects/car2/Program.cs
@@ -4,12 +4,33 @@ namespace car2
 {
     class Program
     {
+        public class Vehicle
+        {
+            public string Make {get;}
+            public string Model {get;}
+            public int Year {get;}
+
+            public Vehicle(string make, string model, int year)
+            {
+                Make = make;
+                Model = model;
+                Year = year;
+            }
+
+            public override string ToString()
+            {
+                return "{Make} {Model}, {Year}";
+            }
+        }
+
+        public class Car
+        {
+            
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             Console.ReadLine();
         }
     }
-
-    
 }
