86ca66d4bb476b8e9903869f37fe4bef01509077
Aaro Haikonen <aaro.haikonen99@gmail.com>
2020-09-04 19:39:44 +0300

    Added Car object

--- ExerciseProjects/car2/Program.cs
+++ ExerciseProjects/car2/Program.cs
@@ -4,19 +4,18 @@ namespace car2
 {
     class Program
     {
-        public abstract class Vehicle
-        {
-            public string Make {get;}
-            public string Model {get;}
-            public int Year {get;}
-
-            enum Types 
+        enum VechileTypes 
             {
                 car,
                 bike,
                 submarine,
                 plane
             }
+        public abstract class Vehicle
+        {
+            public string Make {get;}
+            public string Model {get;}
+            public int Year {get;}
 
             public Vehicle(string make, string model, int year)
             {
@@ -27,13 +26,39 @@ namespace car2
 
             public override string ToString()
             {
-                return "{Make} {Model}, {Year}";
+                return $"{Make} {Model}, {Year}";
             }
+        }
+
+        public class Car : Vehicle
+        {
+            public int TankSize;
+            
+            public int Usage;
+            public int Odometer;
+
+            public Car(string make, string model, int year, int tank_size, int usage) : base(make, model, year)
+            {
+                TankSize = tank_size;
+                Usage = TankSize;
+                Odometer = 0;
+            }
+
+            public void drive(float distance)
+            {
+                place_ =+ distance;
+            }
+
+            // Private parts ;)
+            private float place_ = 0;
+            private float gasLeft_ = 0;
+
         }
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Car audi = new Car("Audi", "A6", 20, 70, 7);
+            Console.WriteLine(audi);
             Console.ReadLine();
         }
     }
